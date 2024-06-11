47c697025b65c0ef72ae0755bb0835bbca097938
Skgwd <skgwd2021@aliyun.com>
2024-06-11 16:27:57 +0800

    add 爱吃香蕉的珂珂(二分查找)

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"leet-code/lib"
 	"math/bits"
 	"slices"
+	"sort"
 	"strings"
 )
 
@@ -132,6 +133,16 @@ func main() {
 	nums = []int{3, 2, 1, 5, 6, 4}
 	// nums = []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
 	fmt.Println("findKthLargest:", findKthLargest(nums, 2)) // 5
+
+	fmt.Println("----- 爱吃香蕉的珂珂(数组,二分查找) ------")
+	// 这里有 n 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 h 小时后回来。
+	// 珂珂可以决定她吃香蕉的速度 k (单位:根/小时)。每个小时，她将会选择一堆香蕉，从中吃掉 k 根。
+	// 如果这堆香蕉少于 k 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+	// 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+	// 返回她可以在 h 小时内吃掉所有香蕉的最小速度 k(k 为整数)。
+	piles := []int{30, 11, 23, 4, 20}
+	h := 6
+	fmt.Println("minEatingSpeed:", minEatingSpeed(piles, h))
 
 }
 
@@ -626,4 +637,15 @@ func quickSelect(nums []int, left, right, k int) int {
 func findKthLargest(nums []int, k int) int {
 	n := len(nums)
 	return quickSelect(nums, 0, n-1, n-k)
+}
+
+func minEatingSpeed(piles []int, h int) int {
+	return 1 + sort.Search(slices.Max(piles)-1, func(mid int) bool {
+		mid++
+		sum := len(piles)
+		for _, p := range piles {
+			sum += (p - 1) / mid
+		}
+		return sum <= h
+	})
 }
